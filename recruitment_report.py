088ac223309edbb95d26058a0e512d5df28206ea
naviantar-ptdh <navi.antar@ptdh.co.id>
2026-04-29 16:43:37 +0700

    Update recruitment_report.py

--- recruitment_report.py
+++ recruitment_report.py
@@ -180,7 +180,7 @@ def run_tracking():
             if v == "CLOSE": return "color: green; font-weight: bold;"
             return ""
 
-        disp = filtered[["candidate_id","position_name","departement","level","loc","status1","last_progress"]].copy()
+        disp = filtered[["candidate_id","position_name","departement","level","loc","last_progress","total_lt","status1"]].copy()
         disp = disp.rename(columns={"status1": "Hiring Status"})
         st.dataframe(disp.style.map(color_st, subset=["Hiring Status"]), use_container_width=True)
         s4, s1, s2, s3 = st.columns(4)
